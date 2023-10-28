acf774e6272ec054a2e51e0e7c1aaa54490c08d3
Dika Wicaksono <dhkwck00@gmail.com>
2023-10-28 12:57:28 +0700

    Initialize new project using create React App

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return <div className="App">
+    <h2>Halo</h2>
+    <Menu/>
+  </div>;
+}
+
+function Menu() {
+  return <div className="Menu">
+    <h1>GOD'S MENU</h1>
+  </div>
+}
+
+export default App;
